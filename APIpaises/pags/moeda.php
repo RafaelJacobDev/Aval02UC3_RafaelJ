fe27ece6f71499db65b81c933f154541ed661656
RAFAEL JACOB TEIXEIRA GRANGEIRO <rafael.jtgrangeiro@senacsp.edu.br>
2025-09-25 18:02:27 -0300

    traformar dentro do php em maiusculo

--- APIpaises/pags/moeda.php
+++ APIpaises/pags/moeda.php
@@ -1,30 +1,43 @@
 <?php
 $moeda = filter_input(INPUT_GET, "moeda");
+$meuarray="";
 
 $url = "https://restcountries.com/v3.1/currency/{$moeda}";
 $options = [
-    "http" => [
+    "https" => [
         "method" => "GET",
         "header" => "Content-Type: application/json"
     ]
 ];
 $context  = stream_context_create($options);
+//var_dump($moeda);
 $response = file_get_contents($url, false, $context);
 
-if ($response === false) {
-    die("Erro ao acessar a API");
-}
 
+if ($response == false) {
+    "Erro ao acessar a API";
+}
 $dados = json_decode($response, true);
+$moedagrande = strtoupper($moeda);
+
+
+
+
+
+
+
 
 if (!empty($dados)) {
     foreach ($dados as $pais) {
         // nome oficial do país
         $nomePais = $pais['name']['official'];
+        $meuarray=$pais['currencies'];
+        $nomeMoeda = $meuarray['name'];
         // nome da moeda para esse país
-        $nomeMoeda = $pais['currencies'][$moeda]['name'] ?? 'Nome de moeda não encontrado';
+        var_dump($meuarray);
 
-        echo "País: {$nomePais} - Moeda: {$nomeMoeda}<br>";
+
+        echo "País: {$nomePais} - Moeda: {$nomeMoeda }<br>";
     }
 }
 ?>
@@ -35,7 +48,7 @@ if (!empty($dados)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultado - Busca CEP</title>
+    <title>Resultado - Busca Moeda</title>
     <link rel="stylesheet" href=".././css/estilo.css">
 </head>
 
